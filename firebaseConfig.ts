0f47492b3a853e2b63d1258dfc56575e0fed1e61
wannn-sion95 <wannnsion95@gmail.com>
2025-10-27 18:07:55 +0700

    update, firebase

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -7,8 +7,8 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.Next_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.Next_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
