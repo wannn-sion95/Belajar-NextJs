20e71d7306077bcaea907392e6f71884f6ad11e4
wannn-sion95 <wannnsion95@gmail.com>
2025-10-28 15:20:39 +0700

    finally

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "",
+        hostname: "**.firebasestorage.googleapis.com",
         pathname: "/**",
       },
     ],
